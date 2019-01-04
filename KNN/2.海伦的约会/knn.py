f5a0524646ee6d2bc71d021aab03c858d9601168
yankang <yankang02@fangdd.com>
2019-01-04 15:32:14 +0800

    2.海伦的约会

--- KNN/2.海伦的约会/knn.py
+++ KNN/2.海伦的约会/knn.py
@@ -15,8 +15,6 @@ Parameters:
 	k - kNN算法参数,选择距离最小的k个点
 Returns:
 	sortedClassCount[0][0] - 分类结果
-Modify:
-	2017-03-24
 """
 def classify0(inX, dataSet, labels, k):
         #numpy函数shape[0]返回dataSet的行数
@@ -34,11 +32,11 @@ def classify0(inX, dataSet, labels, k):
         #定一个记录类别次数的字典
         classCount = {}
         for i in range(k):
-        	#取出前k个元素的类别
-        	voteIlabel = labels[sortedDistIndices[i]]
-        	#dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
-        	#计算类别次数
-        	classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+                #取出前k个元素的类别
+            voteIlabel = labels[sortedDistIndices[i]]
+            #dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
+            #计算类别次数
+            classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
         #python3中用items()替换python2中的iteritems()
         #key=operator.itemgetter(1)根据字典的值进行排序
         #key=operator.itemgetter(0)根据字典的键进行排序
@@ -98,21 +96,21 @@ Returns:
 	minVals - 数据最小值
 """
 def autoNorm(dataSet):
-	    #获得数据的最小值
-	    minVals = dataSet.min(0)
-	    maxVals = dataSet.max(0)
-	    #最大值和最小值的范围
-	    ranges = maxVals - minVals
-	    #shape(dataSet)返回dataSet的矩阵行列数
-	    normDataSet = np.zeros(np.shape(dataSet))
-	    #返回dataSet的行数
-	    m = dataSet.shape[0]
-	    #原始值减去最小值
-	    normDataSet = dataSet - np.tile(minVals, (m, 1))
-	    #除以最大和最小值的差,得到归一化数据
-	    normDataSet = normDataSet / np.tile(ranges, (m, 1))
-	    #返回归一化数据结果,数据范围,最小值
-	    return normDataSet, ranges, minVals
+		#获得数据的最小值
+		minVals = dataSet.min(0)
+		maxVals = dataSet.max(0)
+		#最大值和最小值的范围
+		ranges = maxVals - minVals
+		#shape(dataSet)返回dataSet的矩阵行列数
+		normDataSet = np.zeros(np.shape(dataSet))
+		#返回dataSet的行数
+		m = dataSet.shape[0]
+		#原始值减去最小值
+		normDataSet = dataSet - np.tile(minVals, (m, 1))
+		#除以最大和最小值的差,得到归一化数据
+		normDataSet = normDataSet / np.tile(ranges, (m, 1))
+		#返回归一化数据结果,数据范围,最小值
+		return normDataSet, ranges, minVals
 
 """
 函数说明:可视化数据
@@ -123,63 +121,63 @@ Returns:
 	无
 """
 def showdatas(datingDataMat, datingLabels):
-	    #设置汉字格式
-	    font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
-	    #将fig画布分隔成1行1列,不共享x轴和y轴,fig画布的大小为(13,8)
-	    #当nrow=2,nclos=2时,代表fig画布被分为四个区域,axs[0][0]表示第一行第一个区域
-	    fig, axs = plt.subplots(nrows=2, ncols=2,sharex=False, sharey=False, figsize=(13,8))
+		#设置汉字格式
+		font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=14)
+		#将fig画布分隔成1行1列,不共享x轴和y轴,fig画布的大小为(13,8)
+		#当nrow=2,nclos=2时,代表fig画布被分为四个区域,axs[0][0]表示第一行第一个区域
+		fig, axs = plt.subplots(nrows=2, ncols=2,sharex=False, sharey=False, figsize=(13,8))
 
-	    numberOfLabels = len(datingLabels)
-	    LabelsColors = []
-	    for i in datingLabels:
-	    	if i == 1:
-	    		LabelsColors.append('black')
-	    	if i == 2:
-	    		LabelsColors.append('orange')
-	    	if i == 3:
-	    		LabelsColors.append('red')
-	    #画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第二列(玩游戏)数据画散点数据,散点大小为15,透明度为0.5
-	    axs[0][0].scatter(x=datingDataMat[:,0], y=datingDataMat[:,1], color=LabelsColors,s=15, alpha=.5)
-	    #设置标题,x轴label,y轴label
-	    axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比',FontProperties=font)
-	    axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数',FontProperties=font)
-	    axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占比',FontProperties=font)
-	    plt.setp(axs0_title_text, size=9, weight='bold', color='red')
-	    plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
-	    plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
+		numberOfLabels = len(datingLabels)
+		LabelsColors = []
+		for i in datingLabels:
+			if i == 1:
+				LabelsColors.append('black')
+			if i == 2:
+				LabelsColors.append('orange')
+			if i == 3:
+				LabelsColors.append('red')
+		#画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第二列(玩游戏)数据画散点数据,散点大小为15,透明度为0.5
+		axs[0][0].scatter(x=datingDataMat[:,0], y=datingDataMat[:,1], color=LabelsColors,s=15, alpha=.5)
+		#设置标题,x轴label,y轴label
+		axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比',FontProperties=font)
+		axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数',FontProperties=font)
+		axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占比',FontProperties=font)
+		plt.setp(axs0_title_text, size=9, weight='bold', color='red')
+		plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
+		plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
 
-	    #画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
-	    axs[0][1].scatter(x=datingDataMat[:,0], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
-	    #设置标题,x轴label,y轴label
-	    axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰激淋公升数',FontProperties=font)
-	    axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数',FontProperties=font)
-	    axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰激淋公升数',FontProperties=font)
-	    plt.setp(axs1_title_text, size=9, weight='bold', color='red')
-	    plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
-	    plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
+		#画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+		axs[0][1].scatter(x=datingDataMat[:,0], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
+		#设置标题,x轴label,y轴label
+		axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰激淋公升数',FontProperties=font)
+		axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数',FontProperties=font)
+		axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰激淋公升数',FontProperties=font)
+		plt.setp(axs1_title_text, size=9, weight='bold', color='red')
+		plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
+		plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
 
-	    #画出散点图,以datingDataMat矩阵的第二(玩游戏)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
-	    axs[1][0].scatter(x=datingDataMat[:,1], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
-	    #设置标题,x轴label,y轴label
-	    axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰激淋公升数',FontProperties=font)
-	    axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比',FontProperties=font)
-	    axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰激淋公升数',FontProperties=font)
-	    plt.setp(axs2_title_text, size=9, weight='bold', color='red')
-	    plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
-	    plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
-	    #设置图例
-	    didntLike = mlines.Line2D([], [], color='black', marker='.',
-                          markersize=6, label='didntLike')
-	    smallDoses = mlines.Line2D([], [], color='orange', marker='.',
-	                      markersize=6, label='smallDoses')
-	    largeDoses = mlines.Line2D([], [], color='red', marker='.',
-	                      markersize=6, label='largeDoses')
-	    #添加图例
-	    axs[0][0].legend(handles=[didntLike,smallDoses,largeDoses])
-	    axs[0][1].legend(handles=[didntLike,smallDoses,largeDoses])
-	    axs[1][0].legend(handles=[didntLike,smallDoses,largeDoses])
-	    #显示图片
-	    plt.show()
+		#画出散点图,以datingDataMat矩阵的第二(玩游戏)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+		axs[1][0].scatter(x=datingDataMat[:,1], y=datingDataMat[:,2], color=LabelsColors,s=15, alpha=.5)
+		#设置标题,x轴label,y轴label
+		axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰激淋公升数',FontProperties=font)
+		axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比',FontProperties=font)
+		axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰激淋公升数',FontProperties=font)
+		plt.setp(axs2_title_text, size=9, weight='bold', color='red')
+		plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
+		plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
+		#设置图例
+		didntLike = mlines.Line2D([], [], color='black', marker='.',
+							markersize=6, label='didntLike')
+		smallDoses = mlines.Line2D([], [], color='orange', marker='.',
+							markersize=6, label='smallDoses')
+		largeDoses = mlines.Line2D([], [], color='red', marker='.',
+							markersize=6, label='largeDoses')
+		#添加图例
+		axs[0][0].legend(handles=[didntLike,smallDoses,largeDoses])
+		axs[0][1].legend(handles=[didntLike,smallDoses,largeDoses])
+		axs[1][0].legend(handles=[didntLike,smallDoses,largeDoses])
+		#显示图片
+		plt.show()
 
 
 def datingClassTest():
